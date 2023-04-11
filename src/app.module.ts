74d159ed3b5bf99ff89061020e70e71ecd5d9055
Dmitry <majest228@gmail.com>
2023-04-11 11:45:06 +0600

    Created Files

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ProductModule } from './product/product.module';
+import { FilesModule } from './files/files.module';
+import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
-  imports: [AuthModule, ProductModule],
+  imports: [AuthModule, ProductModule, FilesModule,  MulterModule.register({
+    dest: './uploads',
+  }),
+  FilesModule,],
   controllers: [AppController],
   providers: [AppService],
 })
